9ccec4ae84afeaae640235ccc6f37c0d2a0f16c9
izirku <izirku@users.noreply.github.com>
2019-08-31 11:38:38 -0500

    --wip-- [skip ci]

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,25 +1,8 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -38,40 +21,84 @@ function first() {
 }
 */
 
-
-
 // Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
-first();
+// var a = 'Hello!'
+// first()
 
-function first() {
-    var b = 'Hi!';
-    second();
+// function first() {
+//   var b = 'Hi!'
+//   second()
 
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
+//   function second() {
+//     var c = 'Hey!'
+//     third()
+//   }
+// }
 
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
-
-
+// function third() {
+//   var d = 'John'
+//   //   console.log(a + b + c + d) // no access to `var b` or `var c`
+//   console.log(a + d)
+// }
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+/*
+ * Regular function call - `this` keyword points at the global object
+ *                          (window in case of browsers)
+ * Method call - `this` keyword points at the object that is calling the method
+ *
+ * `this` keyword is not assigned a value until a function where it's defined
+ * is actually called
+ */
 
+// global execution context
+console.log('`this` in a global execution context:', this) // empty object, why?
 
+calculateAge(1984)
 
+function calculateAge(year) {
+  console.log(2019 - year)
+  console.log('`this` in context of regular function call:', this) // window
+}
 
+var john = {
+  name: 'John',
+  yearOfBirth: 1984,
+  calculateAge: function() {
+    console.log('`this` in a method call:', this) // `this` is `john` object
+    console.log(2019 - this.yearOfBirth)
 
+    function innerFunction() {
+      console.log('`this` in a inner function inside of a method call:', this)
+    }
 
+    innerFunction() // `this` is window
 
+    var innerFatFunction = () => {
+      console.log(
+        '`this` in a inner fat-arrow function inside a method call:',
+        this
+      )
+    }
 
+    innerFatFunction() // `this` is `john` object
+  },
+}
+
+console.log('John object ops:')
+
+john.calculateAge()
+
+var mike = {
+  name: 'Mike',
+  yearOfBirth: 1988,
+}
+
+// method borrowing
+mike.calculateAge = john.calculateAge
+
+console.log('Mike object ops:')
+mike.calculateAge()
--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,101 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice, gameActive
+var diceDOM, scoresDOM, currentScoresDOM, playerPanelsDOM
+
+gameActive = true
+scores = [0, 0]
+roundScore = 0
+activePlayer = 0
+
+diceDOM = document.querySelector('.dice')
+
+scoresDOM = [
+  document.getElementById('score-0'),
+  document.getElementById('score-1'),
+]
+
+currentScoresDOM = [
+  document.getElementById('current-0'),
+  document.getElementById('current-1'),
+]
+
+playerPanelsDOM = [
+  document.querySelector('.player-0-panel'),
+  document.querySelector('.player-1-panel'),
+]
+
+function resetGame() {
+  gameActive = true
+  scores = [0, 0]
+  roundScore = 0
+  activePlayer = 0
+  playerPanelsDOM.forEach(el => {
+    el.classList.remove('active')
+    el.classList.remove('winner')
+  })
+  currentScoresDOM.forEach(el => {
+    el.textContent = 0
+  })
+  scoresDOM.forEach(el => {
+    el.textContent = 0
+  })
+  playerPanelsDOM[activePlayer].classList.add('active')
+  document.getElementById('name-0').textContent = 'Player 1'
+  document.getElementById('name-1').textContent = 'Player 2'
+
+  // CSS manipulation
+  diceDOM.style.display = 'none'
+}
+
+function nextPlayer() {
+  currentScoresDOM[activePlayer].textContent = roundScore = 0
+  activePlayer = activePlayer === 0 ? 1 : 0
+  playerPanelsDOM.forEach(panel => {
+    panel.classList.toggle('active')
+  })
+  diceDOM.style.display = 'none'
+}
+
+document.querySelector('.btn-new').addEventListener('click', resetGame)
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  //   console.log('this:', this) // {}
+  //   console.log('target:', e.target) // btn element
+
+  if (gameActive) {
+    // randor is returning [0..1)
+    dice = Math.floor(Math.random() * 6) + 1
+    diceDOM.style.display = 'block' // visible again
+    diceDOM.src = 'dice-' + dice + '.png'
+    //   document.querySelector('#current-' + activePlayer).textContent = dice
+    if (dice !== 1) {
+      roundScore += dice
+      currentScoresDOM[activePlayer].textContent = roundScore
+    } else {
+      nextPlayer()
+    }
+  }
+})
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+  if (gameActive) {
+    scores[activePlayer] += roundScore
+    scoresDOM[activePlayer].textContent = scores[activePlayer]
+    if (scores[activePlayer] >= 100) {
+      gameActive = false
+      diceDOM.style.display = 'none'
+      playerPanelsDOM[activePlayer].classList.remove('active')
+      playerPanelsDOM[activePlayer].classList.add('winner')
+      document.getElementById('name-' + activePlayer).textContent = 'Winner!'
+    } else {
+      nextPlayer()
+    }
+  }
+})
+
+// document.querySelector('.btn-roll').addEventListener('click', function() {
+//   console.log(this) // this is the button element
+// })
